a484193841682f8c0f6868a1b7095710e105d20b
Alex Gerson <alex.gerson@hotmail.com>
2022-08-06 10:09:33 -0400

    title added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Nav from "./components/Nav";
 import CardWrapper from "./components/CardWrapper";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "FactoryFouR";
+  }, []);
+
   return (
     <div>
       <Nav />
